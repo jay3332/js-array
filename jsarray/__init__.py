4dc70ac5fb0c2dd0c2730d877aefb30f88385329
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-03-28 16:32:45 -0400

    Version 2

--- jsarray/__init__.py
+++ jsarray/__init__.py
@@ -1,13 +1,30 @@
-from pandas.core.common import flatten
+try:
+    from pandas.core.common import flatten
+except ImportError:
+    flatten = None
 
 
 class JSArray(list):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.index_of = self.index
+        self.concat = self.extend
 
     @property
     def length(self):
         return len(self)
+
+    @property
+    def sum(self):
+        """ This is not in Javascript, but it would be useful to have """
+        return sum(self)
+
+    def push(self, *items):
+        self.extend(items)
+        return self
+
+    def slice(self, start=None, end=None):
+        return self[slice(start, None, end)]
 
     def map(self, key):
         return JSArray(key(_) for _ in self)
@@ -26,4 +43,30 @@ class JSArray(list):
         return _result[0]
 
     def flat(self):
+        if not flatten:
+            raise ImportError("The pandas module is required to use this method.")
+
         return JSArray(flatten(self))
+
+    def join(self, delimiter):
+        return delimiter.join(self)
+
+    def some(self, key):
+        return self.find(key) is not None
+
+    def shift(self):
+        return self.pop(0)
+
+    def unshift(self, *elements):
+        for element in elements:
+            self.insert(0, element)
+        return self
+
+    def values(self):
+        return iter(self)
+
+    def find_index(self, key):
+        try:
+            return self.index(self.find(key))
+        except ValueError:
+            return None
